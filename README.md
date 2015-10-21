6ae87cf1bd8706792a1dfefb461eaebc84963fa9
Evan Lin <evanslin@gmail.com>
2015-10-21 17:29:51 +0800

    add basic fun

---
+++ README.md
@@ -0,0 +1,61 @@
+PCP: Post’s Correspondence Problems implement in Golang
+==============
+
+[![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)](https://raw.githubusercontent.com/kkdai/cyk/master/LICENSE)  [![GoDoc](https://godoc.org/github.com/kkdai/pcp?status.svg)](https://godoc.org/github.com/kkdai/pcp)  [![Build Status](https://travis-ci.org/kkdai/cyk.svg?branch=master)](https://travis-ci.org/kkdai/pcp)
+
+
+What is this PCP Problem
+=============
+
+The Post correspondence problem is an undecidable decision problem that was introduced by Emil Post in 1946.[1] Because it is simpler than the halting problem and the Entscheidungsproblem it is often used in proofs of undecidability.
+
+(cited from [wiki](https://en.wikipedia.org/wiki/Post_correspondence_problem))
+ 
+
+Installation and Usage
+=============
+
+
+Install
+---------------
+
+    go get github.com/kkdai/pcp
+
+
+Usage
+---------------
+
+
+
+```go
+
+package main
+
+import (
+    "github.com/kkdai/pcp"
+)
+
+func main() {
+}
+
+```
+
+Inspired By
+=============
+
+- [Coursera: Automata](https://class.coursera.org/automata-004/)
+- [Wiki](https://en.wikipedia.org/wiki/Post_correspondence_problem)
+- [Theory of computation / Post’s Correspondence Problems (PCP)](http://www.slideshare.net/ThamerAlamery/theory-of-computation-presentation-final)
+- [Github:PCPSolver](https://github.com/dcatteeu/PCPSolver)
+- [PCP News](https://webdocs.cs.ualberta.ca/~games/PCP/)
+
+Project52
+---------------
+
+It is one of my [project 52](https://github.com/kkdai/project52).
+
+
+License
+---------------
+
+This package is licensed under MIT license. See LICENSE for details.
--- pcp.go
+++ pcp.go
@@ -1,18 +1,58 @@
 package pcp
 
+import (
+	"errors"
+	"fmt"
+	"strings"
+)
+
+// Domino combine two data together
+// Ex: {"ab"/"a"}  DataA= "ab" , DataB="a"
 type Domino struct {
 	DataA string
 	DataB string
 }
 
+type diff struct {
+	diffCompare int // -1(B is longer), 0(eqal) or 1 (A is longer)
+	diffDomino  string
+}
+
+//Use result and current Dif to store for cyclic checking
+type result struct {
+	potentialResult int
+	currentDiff     diff
+}
+
+type currentStatus struct {
+	savedResult []result
+	saveDominos []Domino
+}
+
+//index:0  String A ,  index:1 String B.
+func (c *currentStatus) getString(index int) string {
+	retString := ""
+
+	for _, result := range c.savedResult {
+		if index == 0 { //DataA
+			retString = retString + c.saveDominos[result.potentialResult].DataA
+		} else { //DataB
+			retString = retString + c.saveDominos[result.potentialResult].DataB
+		}
+	}
+	return retString
+}
+
+// PCP (Post’s Correspondence Problems) contains all input dominos
 type PCP struct {
 	//Public:
 	Dominos []Domino
 
 	//private:
-	resultList     []int
+	resultList     []result
 	combineDominoA string
 	combineDominoB string
+	diffDomino     diff
 }
 
 func contrainPrefix(str1, str2 string) bool {
@@ -42,13 +82,64 @@ func getSubsetPrefix(str1, str2 string) (string, bool) {
 	return str1, findSubset
 }
 
+func (p *PCP) isDominoValid(curState currentStatus, inputDomino Domino) bool {
+	strA := curState.getString(0)
+	strB := curState.getString(1)
+
+	tempA := strA + inputDomino.DataA
+	tempB := strB + inputDomino.DataB
+
+	prefix, exist := getSubsetPrefix(tempA, tempB)
+	if !exist {
+		return false
+	}
+
+	fmt.Println("Domino:", inputDomino, " tempA=", tempA, " tempB=", tempB, " prefix=", prefix)
+	return tempA == prefix || tempB == prefix
+}
+
+func (p *PCP) checkDiff(curState currentStatus) (diff, error) {
+	strA := curState.getString(0)
+	strB := curState.getString(1)
+	//Get which string is longer
+	retDiff := diff{}
+	retDiff.diffCompare = strings.Compare(strA, strB)
+
+	if retDiff.diffCompare == 0 {
+		return diff{}, errors.New("No diff")
+	}
+
+	if retDiff.diffCompare == 1 { //A>B
+		retDiff.diffDomino = strings.TrimPrefix(strA, strB)
+	} else { //A<B
+		retDiff.diffDomino = strings.TrimPrefix(strB, strA)
+	}
+
+	return retDiff, nil
+}
+
+func (p *PCP) applyDomino(curState currentStatus, newDom Domino) currentStatus {
+
+	return currentStatus{}
+}
+
+func (p *PCP) isSolutionCyclic() bool {
+	return false
+}
+
 // Add Domino into PCP, will return the domino index.
 // ex: p.AddDomino("abc","c") = 1, it means index is 1.
 // The index use for solution.
 func (p *PCP) AddDomino(strA, strB string) int {
+	newDom := Domino{DataA: strA, DataB: strB}
+	p.Dominos = append(p.Dominos, newDom)
+	return len(p.Dominos) - 1
 }
 
 // Try to find solution and return index slice if exist result
 // If don't have result error will not be nil
 func (p *PCP) FindSolution() ([]int, error) {
+	//Use first one as initialize (MPCP)
+
+	return nil, nil
 }
